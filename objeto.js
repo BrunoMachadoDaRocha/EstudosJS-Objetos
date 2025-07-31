5ad05fd8c0cefc9ff26390ee5ef055c9645d62c2
bruno <bruno2005rocha@gmail.com>
2025-07-31 19:24:35 -0300

    Aula 1 de objetos em JS

---
+++ objeto.js
@@ -0,0 +1,11 @@
+const listaCPFs = ['12312312312', '12341234123'];
+
+const infosPessoas = ['nome', 'José Silva', 'idade', 32];
+console.log(infosPessoas[1]);
+
+const objPessoa = {
+  nome: 'José Silva',
+  idade: 32,
+  cpf: '12312312312',
+  turma: 'JavaScript'
+}
